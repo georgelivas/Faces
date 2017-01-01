dda18984ca28f780c34ebec37e3c11817d3f9a4c
George Livas <giorgos.livas.99@gmail.com>
2017-01-01 21:58:34 +0200

    start face

---
+++ face1 Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  face1 Extension
+//
+//  Created by Giorgos livas on 31/12/2016.
+//  Copyright © 2016 Giorgos livas. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ face1 Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  face1 Extension
+//
+//  Created by Giorgos livas on 31/12/2016.
+//  Copyright © 2016 Giorgos livas. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
